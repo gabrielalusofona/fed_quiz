c481d122d9988cdb07a2e2204e65d3db98affc57
Gabriela Soares <gabriela.soares@multivision.pt>
2024-09-30 15:25:15 +0100

    Add game.py with all the needed functions. Remove files check, classificacao and pessoa

--- classificacao.py
+++
@@ -1,6 +0,0 @@
-def scores(right_answers):
-
-    total = 5
-
-    final_score = (right_answers / total) * 20
-    return f"{conversao: .2f} correto"
--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import json
+import random
 
 def load_questions(file_path):
     print("Inserting questions into questions_dict...")
@@ -17,24 +18,47 @@ def load_questions(file_path):
     }
 
     # Imprimir el diccionario con formato legible
-    print(json.dumps(question_dict, indent=4, ensure_ascii=False))
+    return question_dict
 
 def get_user_response(questions_dict):
-    answers_dict = {}
-    return answers_dict
+    perguntas_selecionadas = random.sample(list(questions_dict.items()), 5) #isto vai dar q1,q3,q7,q2 etc
+
+    respostas = {}
+
+    for i in perguntas_selecionadas: #o i é um desses q1, q2, etc, que são a key do dicionario
+        questao_idx = i[0]
+        questao_escrita = i[1]['question']
+
+        opcoes_idx = i[1]
+        opcoes_escritas = i[1]['options']
+
+        resposta = input(f"{questao_escrita}\ {opcoes_escritas}\ Introduza a sua resposta: ")
+
+        respostas[questao_idx] = resposta
+
+    return respostas
 
 def check_answers(questions_dict, answers_dict):
     right_answers = 0
+
+    for i in answers_dict: #o i é um desses q1, q2, etc, que são a key do dicionario
+        questao_idx = i[0]
+        answer = answers_dict[i]
+        right_answer = questions_dict[i]['answer'][0]
+        if answer == right_answer:
+            right_answers+=1
     return right_answers
 
 def scores(right_answers):
-    new_value=0
-    return new_value
+    total = 5
+    final_score = (right_answers / total) * 20
+    return final_score
+
 
 if __name__ == "__main__":
     questions = load_questions("questions.json")
     answers = get_user_response(questions)
-    right_answers = check_answers(answers)
+    right_answers = check_answers(questions, answers)
     print("A sua nota é: ", scores(right_answers))
 
 

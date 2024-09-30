710066d77990c405887dd5bbbd1e31823a6c4b8b
Matt0404 <matilde.g.f.rodrigues@gmail.com>
2024-09-30 14:59:57 +0100

    ;-;

--- Pessoa_5.py
+++ Pessoa_5.py
@@ -7,14 +7,14 @@ def check_answers(real, utilizador):
     #real seria uma dicionario com 5 dicionarios (resposta real)
     #utilizador lista 5 keys (resposta utilizador)
     point = 0
-    for a in range (0, 5):
+    for a in range (0, len(real)):
         right_ans =  list(real.values())[a].replace(' ', '').lower()
-        try_ans = list(utilizador.values())[a].replace(' ', '').lower()
-
         right_ans = right_ans["answer"]
-        try_ans = try_ans["answer"]
 
-        if right_ans == try_ans:
-            point +=1
+        for i in range(0, len(utilizador)):
+            if right_ans == utilizador[i][1].replace(' ', '').lower():
+                point +=1
+
 
     return point
+
